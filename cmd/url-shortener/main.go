e8d3b5c32ac4129e7b3835712bdd8a2ce921a83d
Artem G <itemun@gmail.com>
2023-11-05 15:18:59 +0300

    feat: logger

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
+
 	"github.com/itemun/restapi/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
 
-	fmt.Println(cfg)
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting url shortener", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
 
 	// TODO: init logger: slog
 
@@ -17,4 +28,27 @@ func main() {
 	// TODO: init router: chi, render
 
 	// TODO: init server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(
+			os.Stdout,
+			&slog.HandlerOptions{Level: slog.LevelDebug},
+		))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(
+			os.Stdout,
+			&slog.HandlerOptions{Level: slog.LevelDebug},
+		))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(
+			os.Stdout,
+			&slog.HandlerOptions{Level: slog.LevelInfo},
+		))
+	}
+
+	return log
 }
